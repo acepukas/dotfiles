45bd8418d043f8f075eabf1a662be5dd92f60be2
Aaron Cepukas <acepukas@gmail.com>
2017-11-25 04:18:25 -0500

    global config files for javascript completion and linting

--- symlink.sh
+++ symlink.sh
@@ -27,6 +27,8 @@ PATHS=(
   ["$DOTFILES/vim"]="$HOME/.vim"
   ["$DOTFILES/zsh/zshrc"]="$HOME/.zshrc"
   ["$DOTFILES/zsh/oh-my-zsh/themes/agnosterc.zsh-theme"]="$HOME/.oh-my-zsh/custom/themes/agnosterc.zsh-theme"
+  ["$DOTFILES/js/.tern-config"]="$HOME/.tern-config"
+  ["$DOTFILES/js/.eslintrc.json"]="$HOME/.eslintrc.json"
 )
 
 COMMAND=$1
