84a8edd8725e2b74ab541ca37f5c827892912402
Aaron Cepukas <acepukas@gmail.com>
2023-05-26 07:38:50 -0400

    lsp command key map descriptions

--- nvim/lua/plugins/lsp/keymaps.lua
+++ nvim/lua/plugins/lsp/keymaps.lua
@@ -23,23 +23,26 @@ function M.on_attach(client, bufnr)
   cmd('command! LspAddWorkspaceFolder lua vim.lsp.buf.add_workspace_folder()')
   cmd('command! LspRemoveWorkspaceFolder lua vim.lsp.buf.remove_workspace_folder()')
 
-  local opts = { buffer=bufnr, silent=true }
-  local map = vim.keymap.set
+  local opts = { buffer=bufnr }
+  local map = require("util").map
+  local function setOpts(o)
+    return vim.tbl_deep_extend("force", o, opts)
+  end
 
-  map('n', 'gD', ':LspDec<CR>', opts)
-  map('n', 'gd', ':LspDef<CR>', opts)
+  map('n', 'gD', ':LspDec<CR>', setOpts({ desc = "Declaration" }))
+  map('n', 'gd', ':LspDef<CR>', setOpts({ desc = "Definition" }))
   map('n', 'K', ':LspHover<CR>', opts)
-  map('n', 'gi', ':LspImplementation<CR>', opts)
-  map('n', '<leader>wa', 'LspAddWorkspaceFolder<CR>', opts)
-  map('n', '<leader>wr', 'LspRemoveWorkspaceFolder<CR>', opts)
-  map('n', '<leader>D', ':LspTypeDef<CR>', opts)
-  map('n', '<leader>rn', ':LspRename<CR>', opts)
-  map('n', '<leader>ca', ':LspCodeAction<CR>', opts)
-  map('n', 'gr', ':LspRefs<CR>', opts)
-  map('n', '<leader>e', ':LspDiagLine<CR>', opts)
-  map('n', '[d', ':LspDiagPrev<CR>', opts)
-  map('n', ']d', ':LspDiagNext<CR>', opts)
-  map('n', '<leader>l', ':LspDiagSetLocList<CR>', opts)
+  map('n', 'gi', ':LspImplementation<CR>', setOpts({ desc = "Implementation" }))
+  map('n', '<leader>wa', 'LspAddWorkspaceFolder<CR>', setOpts({ desc = "Add workspace Folder" }))
+  map('n', '<leader>wr', 'LspRemoveWorkspaceFolder<CR>', setOpts({ desc = "Remove workspace Folder" }))
+  map('n', '<leader>D', ':LspTypeDef<CR>', setOpts({ desc = "Type definition" }))
+  map('n', '<leader>rn', ':LspRename<CR>', setOpts({ desc = "Rename" }))
+  map('n', '<leader>ca', ':LspCodeAction<CR>', setOpts({ desc = "Code action" }))
+  map('n', 'gr', ':LspRefs<CR>', setOpts({ desc = "References" }))
+  map('n', '<leader>e', ':LspDiagLine<CR>', setOpts({ desc = "Show diagnostic" }))
+  map('n', '[d', ':LspDiagPrev<CR>', setOpts({ desc = "Previous diagnostic" }))
+  map('n', ']d', ':LspDiagNext<CR>', setOpts({ desc = "Next diagnostic" }))
+  map('n', '<leader>l', ':LspDiagSetLocList<CR>', setOpts({ desc = "Dianostics to loclist" }))
 
   require'illuminate'.on_attach(client)
 
