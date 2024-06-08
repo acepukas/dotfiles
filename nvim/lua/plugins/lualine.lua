f526ce124aa0966ceb1e6bbfc38c6442dbfe517f
Aaron Cepukas <acepukas@gmail.com>
2024-06-08 13:58:30 -0400

    lualine plugin for lsp indexing progress added

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,15 +1,18 @@
 return {
   {
-    "nvim-lualine/lualine.nvim",
+    'nvim-lualine/lualine.nvim',
+    dependencies = {
+      'arkav/lualine-lsp-progress',
+    },
     opts = function()
       return {
         options = {
           theme = 'gruvbox-material',
         },
         sections = {
-          lualine_c = { { 'filename', path = 1 } }
-        }
+          lualine_c = { { 'filename', path = 1 }, 'lsp_progress' },
+        },
       }
-    end
+    end,
   },
 }
