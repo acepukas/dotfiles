fb06468630b75407d8ea018feacd34a40b7a44e5
Aaron Cepukas <acepukas@gmail.com>
2024-06-19 20:15:35 -0400

    using new nvim-cmp window style, formatting

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -14,8 +14,7 @@ return {
       "p00f/clangd_extensions.nvim",
     },
     opts = function()
-
-      vim.opt.completeopt = 'menu,menuone,noselect'
+      vim.opt.completeopt = "menu,menuone,noselect"
 
       -- Function for cmp-path which returns the current working directory
       local cwd = function()
@@ -30,19 +29,28 @@ return {
       local has_words_before = function()
         unpack = unpack or table.unpack
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        return col ~= 0
+          and vim.api
+              .nvim_buf_get_lines(0, line - 1, line, true)[1]
+              :sub(col, col)
+              :match("%s")
+            == nil
       end
 
       local cmp = require("cmp")
-      local luasnip = require'luasnip'
+      local luasnip = require("luasnip")
 
       return {
         snippet = {
           expand = function(args)
-            require'luasnip'.lsp_expand(args.body)
+            require("luasnip").lsp_expand(args.body)
           end,
         },
         preselect = cmp.PreselectMode.None,
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
         mapping = {
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
@@ -67,28 +75,34 @@ return {
           ["<CR>"] = cmp.mapping({
             i = function(fallback)
               if cmp.visible() and cmp.get_active_entry() then
-                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                cmp.confirm({
+                  behavior = cmp.ConfirmBehavior.Replace,
+                  select = false,
+                })
               else
                 fallback()
               end
             end,
             s = cmp.mapping.confirm({ select = true }),
-            c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+            c = cmp.mapping.confirm({
+              behavior = cmp.ConfirmBehavior.Replace,
+              select = false,
+            }),
           }),
-          ["<C-e>"] = cmp.mapping(cmp.mapping.close(), {"i"}),
-          ["<C-q>"] = cmp.mapping(cmp.mapping.abort(), {"i"}),
-          ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {"i"}),
-          ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {"i"}),
-          ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {"i"}),
+          ["<C-e>"] = cmp.mapping(cmp.mapping.close(), { "i" }),
+          ["<C-q>"] = cmp.mapping(cmp.mapping.abort(), { "i" }),
+          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i" }),
+          ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i" }),
+          ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i" }),
         },
         sources = cmp.config.sources({
-          { name = 'nvim_lsp', priority = 8 },
-          { name = 'luasnip', priority = 7 },
-          { name = 'nvim_lua', priority = 5 },
-          { name = 'nvim_lsp_signature_help' },
+          { name = "nvim_lsp", priority = 8 },
+          { name = "luasnip", priority = 7 },
+          { name = "nvim_lua", priority = 5 },
+          { name = "nvim_lsp_signature_help" },
         }, {
-          { name = 'path', priority = 5, option = { get_cwd = cwd } },
-          { name = 'buffer', option = { get_bufnrs = get_bufnrs } },
+          { name = "path", priority = 5, option = { get_cwd = cwd } },
+          { name = "buffer", option = { get_bufnrs = get_bufnrs } },
         }),
         sorting = {
           priority_weight = 1.0,
@@ -101,10 +115,13 @@ return {
             cmp.config.compare.sort_text,
             cmp.config.compare.length,
             cmp.config.compare.order,
-          }
+          },
         },
         formatting = {
-          format = require'lspkind'.cmp_format({with_text = true, maxwidth = 50})
+          format = require("lspkind").cmp_format({
+            with_text = true,
+            maxwidth = 50,
+          }),
         },
       }
     end,
