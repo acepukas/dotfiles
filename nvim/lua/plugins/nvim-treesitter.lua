263d6c413e8b876f0ecb7ab9433eb87e7eb5f9ef
Aaron Cepukas <acepukas@gmail.com>
2024-06-19 20:33:01 -0400

    removed incremental selection config due to conflict

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -12,7 +12,8 @@ return {
       {
         "nvim-treesitter/nvim-treesitter-textobjects",
         init = function()
-          local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
+          local plugin =
+            require("lazy.core.config").spec.plugins["nvim-treesitter"]
           local opts = require("lazy.core.plugin").values(plugin, "opts", false)
           local enabled = false
           if opts.textobjects then
@@ -24,14 +25,12 @@ return {
             end
           end
           if not enabled then
-            require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+            require("lazy.core.loader").disable_rtp_plugin(
+              "nvim-treesitter-textobjects"
+            )
           end
         end,
       },
-    },
-    keys = {
-      { "<C-space>", desc = "Increment selection" },
-      { "<bs>", desc = "Decrement selection", mode = "x" },
     },
     opts = {
       highlight = {
@@ -40,57 +39,49 @@ return {
       },
       indent = { enable = true },
       ensure_installed = {
-        'bash',
-        'c',
-        'cmake',
-        'comment',
-        'cpp',
-        'css',
-        'dockerfile',
-        'graphql',
-        'go',
-        'gomod',
-        'haskell',
-        'html',
-        'javascript',
-        'jsdoc',
-        'json',
-        'lua',
-        'php',
-        'python',
-        'regex',
-        'scss',
-        'sql',
-        'toml',
-        'tsx',
-        'typescript',
-        'vim',
-        'yaml',
-        'markdown',
-        'markdown_inline',
-        'query',
-        'gotmpl',
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
+        "bash",
+        "c",
+        "cmake",
+        "comment",
+        "cpp",
+        "css",
+        "dockerfile",
+        "graphql",
+        "go",
+        "gomod",
+        "haskell",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "lua",
+        "php",
+        "python",
+        "regex",
+        "scss",
+        "sql",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "gotmpl",
       },
     },
     config = function(_, opts)
       -- treesitter setup for golang template files
-      local parser_config = require'nvim-treesitter.parsers'.get_parser_configs()
+      local parser_config =
+        require("nvim-treesitter.parsers").get_parser_configs()
       parser_config.gotmpl = {
         install_info = {
           url = "https://github.com/ngalaiko/tree-sitter-go-template",
-          files = {"src/parser.c"}
+          files = { "src/parser.c" },
         },
         filetype = "gotmpl",
-        used_by = {"gohtmltmpl", "gotexttmpl", "gotmpl", "yaml"}
+        used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl", "yaml" },
       }
 
       -- load language parser only if not already available
@@ -106,6 +97,6 @@ return {
       end
 
       require("nvim-treesitter.configs").setup(opts)
-    end
+    end,
   },
 }
