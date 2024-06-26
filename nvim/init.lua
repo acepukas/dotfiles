56032c7ff965adf155f9dc6cb35f945b8f24a135
Aaron Cepukas <acepukas@gmail.com>
2024-06-26 07:51:17 -0400

    Plugins Update, improve lazy.nvim init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,13 +4,14 @@ require("config.options")
 
 -- initialize lazy.nvim system requirements
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
+    lazyrepo,
     lazypath,
   })
 end
@@ -19,9 +20,10 @@ vim.opt.rtp:prepend(lazypath)
 -- load lazy.nvim
 require("lazy").setup("plugins", {
   dev = {
-    path = "~/dev/neovim_plugins"
+    path = "~/dev/neovim_plugins",
   },
   install = { colorscheme = { "gruvbox-material", "habamax" } },
+  checker = { enabled = true },
 })
 
 require("config.keymapping")
