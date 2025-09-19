6ffb4455c856544e56923e1b38d6307c15638e2c
Aaron Cepukas <acepukas@gmail.com>
2025-09-19 16:09:36 -0400

    using newer tree-sitter injection syntax for sql in go

--- nvim/after/queries/go/injections.scm
+++ nvim/after/queries/go/injections.scm
@@ -1,15 +1,29 @@
 ; extends
 
+((comment) @injection.language (#offset! @injection.language 0 3 0 0)
 (short_var_declaration
   left: (expression_list
-          (identifier) @_id (#eq? @_id "query"))
+          (identifier))
   right: (expression_list
-           (raw_string_literal) @sql (#offset! @sql 0 1 0 -1))
-)
+           (raw_string_literal
+             (raw_string_literal_content) @injection.content))
+))
 
+((comment) @injection.language (#offset! @injection.language 0 3 0 0)
 (assignment_statement
   left: (expression_list
-          (identifier) @_id (#eq? @_id "query"))
+          (identifier))
   right: (expression_list
-           (raw_string_literal) @sql (#offset! @sql 0 1 0 -1))
+           (raw_string_literal
+             (raw_string_literal_content) @injection.content))
+))
+
+((comment) @injection.language (#offset! @injection.language 0 3 0 0)
+(const_declaration
+  (const_spec
+    name: (identifier)
+    type: (type_identifier)
+    value: (expression_list
+      (raw_string_literal
+        (raw_string_literal_content) @injection.content))))
 )
