10782a8c0c7588e1e51a773b4254b6c563d97951
Aaron Cepukas <acepukas@gmail.com>
2025-09-19 17:16:09 -0400

    nvim-dap configuration

--- nvim/lua/plugins/debugging.lua
+++ nvim/lua/plugins/debugging.lua
@@ -1,3 +1,7 @@
+local getProgName = function()
+  return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+end
+
 return {
   "mfussenegger/nvim-dap",
   dependencies = {
@@ -9,6 +13,45 @@ return {
     local dap, dapui = require("dap"), require("dapui")
     dapui.setup()
     require("dap-python").setup("python")
+    dap.adapters.cppdbg = {
+      id = "cppdbg",
+      type = "executable",
+      command = "/usr/share/cpptools-debug/bin/OpenDebugAD7",
+    }
+    dap.configurations.cpp = {
+      {
+        name = "Launch file",
+        type = "cppdbg",
+        request = "launch",
+        program = getProgName,
+        cwd = "${workspaceFolder}",
+        stopAtEntry = true,
+        setupCommands = {
+          {
+            text = "-enable-pretty-printing",
+            description = "enable pretty printing",
+            ignoreFailures = false,
+          },
+        },
+      },
+      {
+        name = "Attach to gdb server :1234",
+        type = "cppdbg",
+        request = "launch",
+        MIMode = "gdb",
+        miDebuggerServerAddress = "localhost:1234",
+        miDebuggerPath = "/usr/bin/gdb",
+        cwd = "${workspaceFolder}",
+        program = getProgName,
+        setupCommands = {
+          {
+            text = "-enable-pretty-printing",
+            description = "enable pretty printing",
+            ignoreFailures = false,
+          },
+        },
+      },
+    }
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
     end
